3ef1c682683dd9151070fc80f70ba9face7da2f6
Oksana Ovsianyk <kseniaovsianyk@gmail.com>
2024-04-15 13:54:52 +0300

    added 2 new forms and a new interface

--- Laboratorka4/Laboratorka2/GUI.cpp
+++ Laboratorka4/Laboratorka2/GUI.cpp
@@ -1,11 +1,13 @@
 ﻿#include "GUI.h" // Подключаем файл заголовка для формы
-#include "TaQueues.h" // А это уже файл заголовка для алгоритмов очереди
-
+// А это уже файл заголовка для алгоритмов
+#include "TaQueues.h"
+#include "PointerForm.h"
+#include "ArrayForm.h"
 
 using namespace System; // Нужные пространства имен для работы с формами и не только
 using namespace System::Windows::Forms;
 
-#define QUEUE_SIZE 25 // Добавил изменяемость для размера очереди 
+
 
 
 [STAThreadAttribute]
@@ -20,116 +22,12 @@ void main(array<String^>^ args)
 
 System::Void Laboratorka4::MainForm::button_arr_Click(System::Object^ sender, System::EventArgs^ e)
 {
-    listBox1->Items->Clear();
-    listBox2->Items->Clear();
-    ArrQueue names;
-    unsigned long int totalTime = 0;
-
-    for (int i = 1; i <= QUEUE_SIZE; ++i) {
-        String^ message = "The teacher is checking assignment " + i.ToString();
-        listBox1->Items->Add(message);
-        
-
-        names.Enqueue(i);
-        std::string queueOutput = "Queue: ";
-        unsigned short queueSize = names.getSize();
-        unsigned short* queueArray = names.GetQueue();
-        for (unsigned short j = 0; j < queueSize; ++j) {
-            queueOutput += std::to_string(queueArray[j]) + " ";
-        }
-
-        listBox1->Items->Add(gcnew System::String(queueOutput.c_str()));
-        
-
-        totalTime += names.ExecTime();
-    }
-
-    label_time1->Text = "Execution Time: " + totalTime.ToString() + "ns";
-    
-
-    totalTime = 0;
-
-    for (int i = 1; i <= QUEUE_SIZE; ++i) {
-        unsigned short removed_item = names.Dequeue();
-
-        String^ message = "Assignments marked in Campus: " + System::Convert::ToString(removed_item);
-        listBox2->Items->Add(message);
-        std::string queueOutput = "Queue: ";
-        unsigned short queueSize = names.getSize();
-        unsigned short* queueArray = names.GetQueue();
-        for (unsigned short j = 0; j < queueSize; ++j) {
-            queueOutput += std::to_string(queueArray[j]) + " ";
-        }
-
-        listBox2->Items->Add(gcnew System::String(queueOutput.c_str()));
-        
-
-        totalTime += names.ExecTime();
-    }
-
-    label_time2->Text = "Execution Time: " + totalTime.ToString() + "ns";
-    
+    ArrayForm^ arrayForm = gcnew ArrayForm();
+    arrayForm->Show();
 }
 
 System::Void Laboratorka4::MainForm::button_point_Click(System::Object^ sender, System::EventArgs^ e)
 {
-        listBox1->Items->Clear();
-        listBox2->Items->Clear();
-        NodeQueue names;
-        unsigned long int totalTime = 0;
-
-        for (int i = 1; i <= QUEUE_SIZE; ++i) {
-            String^ message = "The teacher is checking assignment " + i.ToString();
-            listBox1->Items->Add(message);
-            
-
-            names.Enqueue(i);
-            std::string queueOutput = "Queue: ";
-            unsigned short queueSize = names.getSize();
-            unsigned short* queueArray = names.GetQueue();
-            for (unsigned short j = 0; j < queueSize; ++j) {
-                queueOutput += std::to_string(queueArray[j]) + " ";
-            }
-
-            listBox1->Items->Add(gcnew System::String(queueOutput.c_str()));
-            
-
-            totalTime += names.ExecTime();
-        }
-
-       
-        label_time1->Text = "Execution Time: " + totalTime.ToString() + "ns";
-        
-    
-         totalTime = 0;
-
-        for (int i = 1; i <= QUEUE_SIZE; ++i) {
-            unsigned short removed_item = names.Dequeue();
-
-            String^ message = "Assignments marked in Campus: " + System::Convert::ToString(removed_item);
-            listBox2->Items->Add(message);
-            
-
-            std::string queueOutput = "Queue: ";
-            unsigned short queueSize = names.getSize();
-            unsigned short* queueArray = names.GetQueue();
-            for (unsigned short j = 0; j < queueSize; ++j) {
-                queueOutput += std::to_string(queueArray[j]) + " ";
-            }
-
-            listBox2->Items->Add(gcnew System::String(queueOutput.c_str()));
-            
-
-            totalTime += names.ExecTime();
-        }
-
-        label_time2->Text = "Execution Time: " + totalTime.ToString() + "ns";
-}
-
-System::Void Laboratorka4::MainForm::button_clean_Click(System::Object^ sender, System::EventArgs^ e)
-{
-    listBox1->Items->Clear();
-    listBox2->Items->Clear();
-    label_time2->Text = "";
-    label_time1->Text = "";
+    PointerForm^ pointerForm = gcnew PointerForm();
+    pointerForm->Show();
 }
